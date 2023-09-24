5bcfb85e8410d242f70536ac7d46ddc9edebf1f0
amelia2802 <amelia280111dutta@gmail.com>
2023-09-24 21:55:50 +0530

    upgrade version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 export default function Footer(){
-    <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Amelia Dutta</a>.
-    </div>
+    return(
+        <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://www.frontendmentor.io/profile/amelia2802" target="_blank">Amelia Dutta</a>.
+        </div>
+    )
 }
